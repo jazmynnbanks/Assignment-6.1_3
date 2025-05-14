ac0be45314b3720f60420027f60176a91d03f6d5
jazmy <jazmy@JAZ-LAPTOP>
2025-05-14 16:16:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+int[] myArray = { 0, 1, 0, 3, 12 };
+
+int insertPos = 0;
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    if (myArray[i] != 0)
+    {
+        myArray[insertPos] = myArray[i];
+        insertPos++;
+    }
+
+}
+
+for (int i = insertPos; i < myArray.Length; i++)
+{
+    myArray[i] = 0;
+}
+
+foreach (int i in myArray)
+{
+    Console.Write(i + " ");
+}
